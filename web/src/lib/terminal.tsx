9de8547593612c9c1be02be511fb608ba8c6bf12
yskszk63 <yskszk63@gmail.com>
2021-09-04 22:00:32 +0900

    Add web demo page.

---
+++ web/src/lib/terminal.tsx
@@ -0,0 +1,70 @@
+import React, { RefObject } from "react";
+import { Terminal } from "xterm";
+import * as FontFaceObserver from 'fontfaceobserver';
+
+interface Props {
+    input?: ReadableStreamDefaultReader,
+    cols?: number,
+    rows?: number,
+}
+
+interface State {
+    div: RefObject<HTMLDivElement>;
+    term: Terminal,
+    input?: ReadableStreamDefaultReader,
+}
+
+class TerminalComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        const term = new Terminal({
+            cols: props.cols || 80,
+            rows: props.rows || 20,
+            fontFamily: 'IBM Plex Mono',
+        });
+        this.state = {
+            div: React.createRef(),
+            term,
+            input: props.input,
+        }
+    }
+
+    async componentDidMount() {
+        const { term, input, div } = this.state as State;
+        const element = div.current;
+        if (element === null) {
+            throw new Error("no element found.");
+        }
+        if (input == null) {
+            throw new Error("no input initialized.");
+        }
+
+        // ref https://github.com/CoderPad/xterm-webfont/blob/master/src/index.js
+        const font = term.getOption("fontFamily");
+        const regular = new FontFaceObserver(font).load();
+        const bold = new FontFaceObserver(font, { weight: 'bold' }).load();
+        await regular.constructor.all([regular, bold]);
+
+        term.open(element);
+        term.focus();
+
+        (async () => {
+            while (true) {
+                const { value, done } = await input.read();
+                if (done) {
+                    break;
+                }
+                if (value) {
+                    term.write(value);
+                }
+            }
+        })();
+    }
+
+    render() {
+        return <div ref={this.state.div}></div>;
+    }
+}
+
+export default TerminalComponent;
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,99 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import dynamic from "next/dynamic";
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import "xterm/css/xterm.css";
+import { WASI } from "@wasmer/wasi"
+import { WasmFs } from "@wasmer/wasmfs";
+
+const DynamicTerminal = dynamic(() => import("../lib/terminal"), {
+  ssr: false
+});
+
+export default function Home() {
+  const Terminal = useMemo(() => DynamicTerminal, []);
+
+  const [rxtx, setRxtx] = useState<{rx?: ReadableStreamDefaultReader, tx?: Function}>({});
+  const [wasm, setWasm] = useState<WebAssembly.Module|undefined>();
+  const [[cols, rows], ] = [[88, 30], ];
+  const prompt = "$ ";
+
+  useEffect(() => {
+    let controller: ReadableStreamDefaultController | null = null;
+    const rx = new ReadableStream({
+      start(c) {
+        controller = c;
+      }
+    });
+    const tx = (chunk: string) => {
+        if (controller !== null) {
+          controller.enqueue(chunk);
+        }
+      };
+    setRxtx({rx: rx.getReader(), tx, });
+    tx(prompt);
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+        const module = await WebAssembly.compileStreaming(fetch("imcat_rs.wasm"));
+        setWasm(module);
+    })();
+  }, []);
+
+  const oninput = useCallback(async (evt) => {
+    if (evt.target.files.length && rxtx && rxtx.tx && wasm) {
+        const [file] = evt.target.files;
+
+        const fs = new WasmFs();
+        const wasi = new WASI({
+            args: [],
+            env: {},
+            bindings: {
+                ...WASI.defaultBindings,
+                fs: fs.fs,
+            }
+        });
+        const instance = await WebAssembly.instantiate(wasm, {
+            ...wasi.getImports(wasm),
+        });
+        wasi.start(instance);
+
+        const unkoResponse = await fetch(URL.createObjectURL(file));
+        if (unkoResponse.status != 200) {
+            throw new Error("failed to fetch unko");
+        }
+        const unkoPng = await unkoResponse.arrayBuffer();
+
+        await new Promise(resolve => {
+            fs.fs.writeFile("/dev/stdin", new Uint8Array(unkoPng), resolve);
+        });
+        (instance.exports.wasi_imcat as Function)(0, 1, cols, rows);
+        rxtx.tx("imcat_rs -\r\n");
+
+        const data = await fs.getStdOut();
+        rxtx.tx(data);
+        rxtx.tx("\r\n");
+        const data2 = fs.fs.readFileSync("/dev/stderr", "utf8");
+        if (data2) {
+            rxtx.tx(data2);
+        }
+        rxtx.tx(prompt);
+    }
+  }, [rxtx, wasm]);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>imcat-rs</title>
+        <meta name="description" content="imcat-rs web demo." />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>○</text></svg>" />
+      </Head>
+
+      <main className={styles.main}>
+        <label>$ imcat_rs <input type="file" onInput={oninput} accept="image/*"/></label>
+        <Terminal input={rxtx.rx} cols={cols} rows={rows}/>
+      </main>
+    </div>
+  )
+}
