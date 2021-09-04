c12fccb47b17d504f5f44d5ed84159c90f0236ea
yskszk63 <yskszk63@gmail.com>
2021-09-04 23:19:24 +0900

    add ogp propery

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -88,6 +88,8 @@ export default function Home() {
         <title>imcat-rs</title>
         <meta name="description" content="imcat-rs web demo." />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>○</text></svg>" />
+        <meta property="og:description" content="imcat Clone. Implemented by Rust. " />
+        <meta property="og:title" content="imcat-rs demo." />
       </Head>
 
       <main className={styles.main}>
